6c69c1c472dd94f06a8b9a7953cbf5e48ee2b184
jay-amin-tops <jay.amin.tops@gmail.com>
2022-11-08 14:58:59 +0530

    Duper Globals

--- PHP/04Operators.php
+++ PHP/04Operators.php
@@ -5,5 +5,14 @@
 // compar 
 $marks = 50;
 //++ --
+$marks = 50;
+if ($marks > 50) {
+    echo "called if";
+} else {
+    echo "called else";
+}
+echo ($marks > 50)? "called if":"called else";
+
+echo  55<=>60; 
 
 ?>
